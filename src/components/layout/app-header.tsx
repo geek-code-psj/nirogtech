874fc4d1ab7eb12bd10fd6a5aa7a378147c1ca33
PRABAL SINGH <email.prabalsingh1@gmail.com>
2025-10-01 20:34:22 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import React from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
