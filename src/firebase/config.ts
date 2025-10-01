8d90e8db7d1aa216bf66e0d2af93c83dc1477414
PRABAL SINGH <email.prabalsingh1@gmail.com>
2025-10-01 20:34:55 +0000

    also setup env file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyB3KXVUJKYBJiksSqeBpAiugCsYn_C0GkQ",
-  authDomain: "nirogtech.firebaseapp.com",
-  projectId: "nirogtech",
-  storageBucket: "nirogtech.firebasestorage.app",
-  messagingSenderId: "924511885285",
-  appId: "1:924511885285:web:99e32033b05ab7353d810c",
-  measurementId: "G-8CGG6EXJ9B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
