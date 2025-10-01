17b153fbf13e4907ef4e662acbbd0096bd8f2c6b
PRABAL SINGH <email.prabalsingh1@gmail.com>
2025-10-01 21:37:08 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,7 +13,7 @@ import { doc, setDoc } from 'firebase/firestore';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Logo } from '@/components/logo';
 import { useToast } from '@/hooks/use-toast';
