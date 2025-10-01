d17b653ea5496efcd39d63a262f5873e926b1b75
PRABAL SINGH <email.prabalsingh1@gmail.com>
2025-10-01 21:29:17 +0000

    do it

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2874782809-23da9",
-  "appId": "1:113225148365:web:9d96c517276cc3ac61c684",
-  "apiKey": "AIzaSyDIoNEv7_giDakKCHicXvbahggEsUzfsBc",
-  "authDomain": "studio-2874782809-23da9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "113225148365"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
